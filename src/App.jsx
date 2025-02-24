4069757d6d3164e74732adbe31d5f6a21750502b
sahith m <mdsah@BUJJI.>
2025-02-24 11:30:46 +0530

    Completed the assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import BookCard from './components/bookcard.jsx';
+
 
 function App() {
   const books = [
@@ -34,7 +35,20 @@ function App() {
     },
   ];
 
-  return <>{/* write code to display all the books using */}</>;
+  return (
+    <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+      {books.map((book) => (
+        <BookCard
+          key={book.id}
+          image={book.image}
+          name={book.name}
+          genre={book.genre}
+          author={book.author}
+        />
+      ))}
+    </div>
+  );
+
 }
 
 export default App;
